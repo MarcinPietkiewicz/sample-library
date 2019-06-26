dd6f2fe44fb33ecf92b2a3748500eff4a3545ee6
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-26 19:21:31 +0200

    Added HTMLGreeting function and created test script to check functionality

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var g = G$("john", "doe");
+var g = G$("John", "Doe");
 console.log(g);
+
+g
+  .greet()
+  .setLang("es")
+  .greet(true)
+  .log().HTML;
+
+$("#login").click(function() {
+  var loginGrtr = G$("Johnathan", "Doe");
+  $("#logindiv").hide();
+
+  loginGrtr
+    .setLang($("#lang").val())
+    .HTMLGreeting("#greeting", true)
+    .log();
+});
